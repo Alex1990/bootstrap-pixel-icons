a8667bc9704ea4a4ee8e5da2db40020358c66ace
Alex Chao <alexchao1990@gmail.com>
2021-01-23 17:12:49 +0800

    feat: generate css of icons (#4)

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
+const Async = require('async')
 const gulp = require('gulp')
 const iconfont = require('gulp-iconfont')
+const consolidate = require('gulp-consolidate')
+
 const runTimestamp = Math.round(Date.now() / 1000)
 
-gulp.task('iconfont', () => {
-  return gulp.src(['svg/*.svg'])
+gulp.task('iconfont', (done) => {
+  const iconStream = gulp.src(['svg/*.svg'])
     .pipe(iconfont({
       fontName: 'bootstrap-pixel-icons',
       formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
       timestamp: runTimestamp
     }))
-    .pipe(gulp.dest('fonts/'))
+
+  Async.parallel([
+    cb => {
+      iconStream.on('glyphs', (glyphs, options) => {
+        gulp.src('templates/bootstrap-pixel-icons.css')
+          .pipe(consolidate('lodash', {
+            glyphs,
+            fontName: 'bootstrap-pixel-icons',
+            fontPath: './fonts/',
+            className: 'px'
+          }))
+          .pipe(gulp.dest('./'))
+          .on('finish', cb)
+      })
+    },
+    cb => {
+      iconStream
+        .pipe(gulp.dest('fonts/'))
+        .on('finish', cb)
+    }
+  ], done)
 })
